a128087e701f4f26d9f01fc230cb7f25f1f58255
KunigiMohan <kmohanr5@gmail.com>
2021-12-17 18:54:00 +0530

    Added : Using PUT request method to firstName as path variable and lastName as Query Parameter.

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -2,15 +2,23 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.User;
 
 /**
  * @RequestMapping : to set URL path
  * @org.springframework.web.bind.annotation.RestController : RestController
  * @RequestParam:to pass the value of a parameter.
  * @GetMapping : to pass the value of a parameter.
+ * @PostMapping : to post the firstName and lastName parameter to user class
+ * @RequestedBody : getting object of user class
+ * @PathVariable : extracting variables localhost URI
  */
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hello")
@@ -28,6 +36,16 @@ public class RestController {
 	@GetMapping ("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + " ! ";
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() +" ! ";
+	}
+	
+	@PutMapping("/put/{firstName}")
+	public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName) {
+		return "Hello "+ firstName + " "+ lastName + "!";
 	}
 
 }
