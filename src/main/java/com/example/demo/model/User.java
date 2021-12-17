d1754bac9709ce9d169c8489d1ba93f8f663eea4
KunigiMohan <kmohanr5@gmail.com>
2021-12-17 18:12:09 +0530

    Added : Creating class for User DTO Bean.

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+/**
+ * 
+ * Class containing user detail variables
+ *
+ */
+public class User {
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {			//getters and setters for getting and initializing variables 
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
